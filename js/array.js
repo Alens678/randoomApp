464957677c1b8c0b1b219cfb6cc96e70bff77a79
Angela-GM <angelagm0112@gmail.com>
2023-05-11 12:59:26 +0200

    create images cabeza

---
+++ js/array.js
@@ -0,0 +1,27 @@
+const clothes = [
+  {
+    src: "https://i.pinimg.com/originals/19/e3/5c/19e35c3846b9a9fc8f40b75ca8d8e5dc.jpg",
+    alt: "Gorro Mario",
+    clase: "cabeza",
+  },
+  {
+    src: "https://img2.freepng.es/20180807/bri/kisspng-santa-claus-christmas-day-bonnet-party-disguise-5b69d7c176f3c9.3116787415336631694872.jpg",
+    alt: "Gorro Papa Noel",
+    clase: "cabeza",
+  },
+  {
+    src: "https://w7.pngwing.com/pngs/331/944/png-transparent-sailor-cap-hat-sailor-cap-bonnet-cap-hat-wedding-costume-party.png",
+    alt: "Gorro marinero",
+    clase: "cabeza",
+  },
+  {
+    src: "https://img2.freepng.es/20181116/sfj/kisspng-witch-hat-clip-art-image-portable-network-graphics-tube-halloween-chapeau-5bee5efecd2a61.3274511515423485428404.jpg",
+    alt: "Gorro Bruja",
+    clase: "cabeza",
+  },
+  {
+    src: "https://w7.pngwing.com/pngs/700/601/png-transparent-bowler-hat-clown-costume-party-gorro-hat-halloween-costume-top-hat.png",
+    alt: "Gorro Payaso",
+    clase: "cabeza",
+  },
+];
