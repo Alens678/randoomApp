25b74551df2e9e9d0977dbefc76cdaf141d8a2c0
Angela-GM <angelagm0112@gmail.com>
2023-05-12 14:12:03 +0200

    add sections images

--- js/motorRandom.js
+++ js/motorRandom.js
@@ -1,5 +1,5 @@
 // Seleccionar los diferentes articles del DOM
-const carrousel = document.getElementsByClassName("carrousel")
+const carrousel = document.getElementsByClassName("carrousel");
 const cardCabeza = document.getElementById("card-cabeza");
 const cardPecho = document.getElementById("card-pecho");
 const cardPiernas = document.getElementById("card-piernas");
@@ -9,6 +9,28 @@ const cardPies = document.getElementById("card-pies");
 // Declarar un array vaciio donde se guardarán los indices del array original desordenados
 const randomClothes = [];
 
+// Bucle para generar las imágenes y agregarlas a los elementos correspondientes
+for (let i = 0; i < clothes.length; i++) {
+  for (let j = 0; j < clothes[i].length; j++) {
+    const divCard = document.createElement("div");
+    const img = document.createElement("img");
+
+    img.src = clothes[i][j].src;
+    img.alt = clothes[i][j].alt;
+
+    divCard.appendChild(img);
+
+    if (i === 0) {
+      cardCabeza.appendChild(divCard);
+    } else if (i === 1) {
+      cardPecho.appendChild(divCard);
+    } else if (i === 2) {
+      cardPiernas.appendChild(divCard);
+    } else if (i === 3) {
+      cardPies.appendChild(divCard);
+    }
+  }
+}
 // Recorrer array original
 /*for (let i = 0; i < clothes.length; i++) {
   let randomClothe = Math.floor(Math.random() * clothes.length);
@@ -23,53 +45,50 @@ const randomClothes = [];
 let exist = false;
 
 const randomImg = () => {
-  let randomClotheIndex = Math.floor(Math.random() * clothes.length);
+  let randomClotheIndexCabeza = Math.floor(Math.random() * clothes[0].length);
+  let randomClotheIndexPecho = Math.floor(Math.random() * clothes[1].length);
+  let randomClotheIndexPiernas = Math.floor(Math.random() * clothes[2].length);
+  let randomClotheIndexPies = Math.floor(Math.random() * clothes[3].length);
   const divCabeza = document.getElementById("select-cabeza");
+  const divPecho = document.getElementById("select-pecho");
+  const divPiernas = document.getElementById("select-piernas");
+  const divPies = document.getElementById("select-pies");
   // Crear elemento img
-  const img = document.createElement("img");
+  const imgCabeza = document.createElement("img");
+  const imgPecho = document.createElement("img");
+  const imgPiernas = document.createElement("img");
+  const imgPies = document.createElement("img");
   // Añadir los atributos a las imagenes
-  img.src = clothes[randomClotheIndex].src;
-  img.alt = clothes[randomClotheIndex].alt;
+  imgCabeza.src = clothes[randomClotheIndexCabeza].src;
+  imgCabeza.alt = clothes[randomClotheIndexCabeza].alt;
+  imgPecho.src = clothes[randomClotheIndexPecho].src;
+  imgPecho.alt = clothes[randomClotheIndexPecho].alt;
+  imgPiernas.src = clothes[randomClotheIndexPiernas].src;
+  imgPiernas.alt = clothes[randomClotheIndexPiernas].alt;
+  imgPies.src = clothes[randomClotheIndexPies].src;
+  imgPies.alt = clothes[randomClotheIndexPies].alt;
 
   // Agregar la imagen al div
-   divCabeza.appendChild(img);
+  divCabeza.appendChild(imgCabeza);
+  divPecho.appendChild(imgPecho);
+  divPiernas.appendChild(imgPiernas);
+  divPies.appendChild(imgPies);
 
-   exist = true;
-}
-
-
-
-// Bucle para generar imagenes
-for (let i = 0; i < clothes.length; i++) {
-   const divCard = document.createElement("div");
-  // Crear elemento img
-  const img = document.createElement("img");
-  // Añadir los atributos a las imagenes
-  img.src = clothes[i].src;
-  img.alt = clothes[i].alt;
-
-  // Agregar la imagen al div
-  divCard.appendChild(img);
-
-  // Agregar el div al artículo
-  cardCabeza.appendChild(divCard);
-}
+  exist = true;
+};
 
 // Funcion para mostar imagen aleatoria y desaparecer carrousel
 const selectRandom = () => {
-  
   // Desaparecer carrousel divCard añadir clase .hidden
-for (let i = 0; i < carrousel.length; i++) {
-  carrousel[i].classList.add("hidden");
-}
+  for (let i = 0; i < carrousel.length; i++) {
+    carrousel[i].classList.add("hidden");
+  }
   // Mostar imagen random
-  if(!exist){
+  if (!exist) {
     randomImg();
   } else {
-    alert("mensaje")
+    alert("mensaje");
   }
-  
-  
 };
 // Evento click
 const btnStart = document.addEventListener("click", selectRandom);
