662cd912ff00aa4c0e84f36c389e1767afc97e0a
Angela-GM <angelagm0112@gmail.com>
2023-05-16 11:38:54 +0200

    cambiar ruta imagen

--- js/array.js
+++ js/array.js
@@ -1,6 +1,31 @@
 const clothes = [
   [
     {
+      src: "assets/img/cabeza/cabeza1.png",
+      alt: "Gorro mago",
+      clase: "cabeza",
+    },
+    {
+      src: "./assets/img/cabeza/cabeza2.png",
+      alt: "Gorro Marinero",
+      clase: "cabeza",
+    },
+    {
+      src: "./assets/img/cabeza/cabeza3.png",
+      alt: "Gorro Papa Noel",
+      clase: "cabeza",
+    },
+    {
+      src: "./assets/img/cabeza/cabeza4.png",
+      alt: "Gorro chef",
+      clase: "cabeza",
+    },
+    {
+      src: "./assets/img/cabeza/caebeza5.png",
+      alt: "Gorro Bruja",
+      clase: "cabeza",
+    },
+    {
       src: "./assets/img/cabeza/cabeza1.png",
       alt: "Gorro mago",
       clase: "cabeza",
@@ -27,6 +52,31 @@ const clothes = [
     },
   ],
   [
+    {
+      src: "./assets/img/pecho/pecho1.png",
+      alt: "Pecho Hulk",
+      clase: "pecho",
+    },
+    {
+      src: "./assets/img/pecho/pecho2.png",
+      alt: "Pecho Bombero ",
+      clase: "pecho",
+    },
+    {
+      src: "./assets/img/pecho/pecho3.png",
+      alt: "Pecho angel ",
+      clase: "pecho",
+    },
+    {
+      src: "./assets/img/pecho/pecho4.png",
+      alt: "Pecho Freddy",
+      clase: "pecho",
+    },
+    {
+      src: "./assets/img/pecho/pecho5.png",
+      alt: "Pecho Freddy",
+      clase: "pecho",
+    },
     {
       src: "./assets/img/pecho/pecho1.png",
       alt: "Pecho Hulk",
@@ -79,8 +129,58 @@ const clothes = [
       alt: "Pantalon sirena",
       clase: "piernas",
     },
+    {
+      src: "./assets/img/piernas/piernas1.png",
+      alt: "Pantalon rosa con puntos blancas",
+      clase: "piernas",
+    },
+    {
+      src: "./assets/img/piernas/piernas2.png",
+      alt: "Pantalon camuflado militar",
+      clase: "piernas",
+    },
+    {
+      src: "./assets/img/piernas/piernas3.png",
+      alt: "Pantalon Aladdin",
+      clase: "piernas",
+    },
+    {
+      src: "./assets/img/piernas/piernas4.png",
+      alt: "Falda Tutu",
+      clase: "piernas",
+    },
+    {
+      src: "./assets/img/piernas/piernas5.png",
+      alt: "Pantalon sirena",
+      clase: "piernas",
+    },
   ],
   [
+    {
+      src: "./assets/img/pie/pie1.png",
+      alt: "Botas piratas",
+      clase: "pies",
+    },
+    {
+      src: "./assets/img/pie/pie2.png",
+      alt: "Zapatos payaso",
+      clase: "pies",
+    },
+    {
+      src: "./assets/img/pie/pie3.png",
+      alt: "Botas texanas",
+      clase: "pies",
+    },
+    {
+      src: "./assets/img/pie/pie4.png",
+      alt: "Botas de goma rosa",
+      clase: "pies",
+    },
+    {
+      src: "./assets/img/pie/pie5.png",
+      alt: "Tacones rojos",
+      clase: "pies",
+    },
     {
       src: "./assets/img/pie/pie1.png",
       alt: "Botas piratas",
