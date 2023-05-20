daed228af049023a09dd1580e67fd7e87e179d2d
sebastian-riggio <sebastian.riggio@gmail.com>
2023-05-20 18:16:15 +0200

    sonidos de like y boton atras

--- js/sonido.js
+++ js/sonido.js
@@ -14,3 +14,20 @@ function reproducirSonido() {
     boton.classList.remove("clicked");
   }, 200); // Restablecer la clase después de 200 milisegundos (ajusta el tiempo según tus necesidades)
 }
+
+
+const boton2 = document.getElementById("miBoton2");
+boton2.addEventListener("click", reproducirSonido2);
+
+
+
+function reproducirSonido2() {
+  var audio = new Audio('bu.mp3');
+  audio.play();
+
+  boton2.classList.add("clicked");
+
+  setTimeout(function() {
+    boton2.classList.remove("clicked");
+  }, 200); // Restablecer la clase después de 200 milisegundos (ajusta el tiempo según tus necesidades)
+}
